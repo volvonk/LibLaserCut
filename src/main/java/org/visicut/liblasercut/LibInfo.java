c27abbdca2591694648b9d021a987f24cb9287bb
Thomas Oster <thomas.oster@rwth-aachen.de>
2013-08-14 13:11:02 +0200

    Change Group-ID from com.t_oster to org.visicut

---
+++ src/main/java/org/visicut/liblasercut/LibInfo.java
@@ -0,0 +1,53 @@
+/**
+ * This file is part of LibLaserCut.
+ * Copyright (C) 2011 - 2013 Thomas Oster <thomas.oster@rwth-aachen.de>
+ * RWTH Aachen University - 52062 Aachen, Germany
+ *
+ *     LibLaserCut is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     LibLaserCut is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with LibLaserCut.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+package org.visicut.liblasercut;
+
+import org.visicut.liblasercut.drivers.EpilogZing;
+import org.visicut.liblasercut.drivers.IModelaMill;
+import org.visicut.liblasercut.drivers.LaosCutter;
+import org.visicut.liblasercut.drivers.Dummy;
+import org.visicut.liblasercut.drivers.EpilogHelix;
+import org.visicut.liblasercut.drivers.Lasersaur;
+
+/**
+ * This class contains Version information
+ * etc from the Library
+ * @author Thomas Oster <thomas.oster@rwth-aachen.de>
+ */
+public class LibInfo
+{
+  private static String VERSION = "visicut1.7";
+  
+  public static String getVersion()
+  {
+    return VERSION;
+  }
+  
+  public static Class[] getSupportedDrivers()
+  {
+    return new Class[]{
+      EpilogZing.class,
+      EpilogHelix.class,
+      LaosCutter.class,
+      Lasersaur.class,
+      Dummy.class,
+      IModelaMill.class
+    };
+  }
+}
